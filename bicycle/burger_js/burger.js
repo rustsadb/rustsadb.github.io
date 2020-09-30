880425ba67a59f4fc4656d0abf9447740e87367b
rustsadb <71855453+rustsadb@users.noreply.github.com>
2020-09-30 15:57:08 +0300

    Delete burger.js

--- bicycle/burger_js/burger.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    $('.burger').click(function(event){
-        $('.burger,.header').toggleClass('active');
-        $('body').toggleClass('lock');
-    }),
-    $('.menu__link').click(function(event){
-        $('.burger,.header').removeClass('active');
-    })
-})
