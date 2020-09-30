0ca3470413139b16be3ab55ba2c0df202995cc2e
rustsadb <71855453+rustsadb@users.noreply.github.com>
2020-09-30 15:44:44 +0300

    Update burger.js

--- bicycle/burger_js/burger.js
+++ bicycle/burger_js/burger.js
@@ -2,5 +2,8 @@ $(document).ready(function(){
     $('.burger').click(function(event){
         $('.burger,.header').toggleClass('active');
         $('body').toggleClass('lock');
+    }),
+        $('.menu__link').click(function(event){
+        $('.burger,.header').removeClass('active');
     })
 })
