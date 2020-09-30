4fd6398f41e40d613b002af633c2259d360c9d0e
rustsadb <71855453+rustsadb@users.noreply.github.com>
2020-09-30 15:59:32 +0300

    Add files via upload

---
+++ bicycle/burger_js/burger.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.burger').click(function(event){
+        $('.burger,.header').toggleClass('active');
+        $('body').toggleClass('lock');
+    }),
+    $('.menu__link').click(function(event){
+        $('.burger,.header').removeClass('active');
+    })
+})
